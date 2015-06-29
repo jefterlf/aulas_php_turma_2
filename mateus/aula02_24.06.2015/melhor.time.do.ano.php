d0c438caddbd93239d5e3d838d88df63696c88af
mateusmotti <mateus.aviz.motti@gmail.com>
2015-06-29 19:05:35 -0400

    aspas, constantes, melhor.time.do.ano

---
+++ mateus/aula02_24.06.2015/melhor.time.do.ano.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+	<head>
+		<meta charset="UTF-8" />
+		<title>AULA 02 25.06.2015</title>
+	</head>
+	<body>
+		<?php
+			$time = "Samsung White";
+			$ano = 2014;
+			$frase1 = "O $time é o melhor time de League Of Legends do mundo!";
+			$frase2 = "O $time foi campeão do mundo em $ano";
+			echo "<h3>$frase1</h3>";
+			echo "<h3>$frase2</h3>";
+		?>
+	</body>
+</html>
