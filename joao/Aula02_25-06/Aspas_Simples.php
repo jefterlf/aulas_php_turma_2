11aed4861dc4e83396a0eb695be790a7650ea766
JoaoJ0 <joaomarcos1933@hotmail.com>
2015-06-29 19:00:45 -0400

    Aula03_25-06

---
+++ joao/Aula02_25-06/Aspas_Simples.php
@@ -0,0 +1,12 @@
+<html>
+<body>
+<?php
+	echo '<p align=center> Texto utilizando aspas simples</p>';
+	echo "welcome to the John's page e<br />";
+	echo "Isto é um test";
+
+	echo 'Coloquei uma quebra de linha';
+
+?>
+</body>
+</html>
---
+++ joao/Aula02_25-06/Constantes.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<?php
+	$texto1 = "Este é  o texto 1";
+	echo $texto1;
+	$texto1 = "Este é o texto 2";
+	echo $texto1;
+
+	define ("meunome","Joao Marcos");
+	define ("peso", 520);
+	echo "O meu nome é " . meunome;
+	echo "<br>";
+	echo "O meu peso é " . peso . " quilos.";
+
+?>
+</body>
+</html>
