75f97e0f2d3abee64e6b0a0e1120de31fbeef5cb
mateusmotti <mateus.aviz.motti@gmail.com>
2015-06-29 20:29:37 -0400

    aulas03

--- mateus/aula03_29.06.2015/exemplo4_7.php
+++ mateus/aula03_29.06.2015/exemplo4_7.php
@@ -5,6 +5,11 @@
         <title>conversao de variaveis</title>
     </head>
     <body>
-        
+        <?php
+            $x = 50;
+            $y = 2.35;
+            $soma = (int) $y + $x;
+            echo $soma;
+        ?>
     </body>
 </html>    
