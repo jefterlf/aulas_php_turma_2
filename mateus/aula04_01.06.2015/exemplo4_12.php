b9f7c27a850827a1f5dcd2ac473edaded4ca828e
mateusmotti <mateus.aviz.motti@gmail.com>
2015-07-01 19:32:24 -0400

    arrays

---
+++ mateus/aula04_01.06.2015/exemplo4_12.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="utf-8">
+        <title>Porto Alegre</title>
+    </head>
+    <body>
+       <?php
+            
+       ?>
+    </body>
+</html>
