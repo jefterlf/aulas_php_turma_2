583558ece4e4faa1e35f02e90bfa236472343553
mateusmotti <mateus.aviz.motti@gmail.com>
2015-07-01 19:04:43 -0400

    campeão da américa

---
+++ mateus/aula04_01.06.2015/exemplo4_8.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8" />
+        <title>Campeão da América</title>
+    </head>
+    <body>
+        <?php
+            $time = "Grêmio";
+            $titulo = "Campeão da América";
+            $ano1 = "1983";
+            $ano2 = "1995";
+            echo "O $time foi $titulo em $ano1 e $ano2";
+        ?>
+    </body>
+</html>
