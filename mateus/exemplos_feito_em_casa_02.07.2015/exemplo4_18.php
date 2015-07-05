66fc12b2948191a9451670be24cf8f09ed7b1f25
mateusmotti <mateus.aviz.motti@gmail.com>
2015-07-04 21:23:40 -0400

    operadores ternários - binários - lógicos e precedência de operadores

---
+++ mateus/exemplos_feito_em_casa_02.07.2015/exemplo4_18.php
@@ -0,0 +1,5 @@
+<?php
+    $num = 5;
+    $resultado = 8 + 3 * 2 + ++$num;
+    echo "$num</br>";
+    echo $resultado;
---
+++ mateus/exemplos_feito_em_casa_02.07.2015/exemplo4_19.php
@@ -0,0 +1,4 @@
+<?php
+    $num = 7;
+    $resultado = 8 * $num % 2;
+    echo $resultado;
