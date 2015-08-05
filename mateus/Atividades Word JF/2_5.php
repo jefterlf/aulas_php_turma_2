ce69d89b0d395276faeac891c20d3340669f59be
mateusmotti <mateus.aviz.motti@gmail.com>
2015-08-05 19:45:21 -0400

    2_5 e 2_6

---
+++ mateus/Atividades Word JF/2_5.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Crescente</title>
+    </head>
+    <body>
+        <form method="post" action="2_5.php">
+            <label>Valor de A:</label>
+            <input type="number" name="numeroA" id="numeroA"/>
+            <label>Valor de B:</label>
+            <input type="number" name="numeroB" id="numeroB"/>
+            <input type="submit" name="enviar" id="enviar"/>
+        </form>
+        <?php
+             if(isset($_POST['enviar'])){
+                 
+             }
+        ?>
+    </body>
+</html>
---
+++ mateus/Atividades Word JF/2_6.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Notas</title>
+    </head>
+    <body>
+        <form method="post" action="2_6.php">
+            
+        </form>
+        <?php
+             if(isset($_POST['enviar'])){
+                 
+             }
+        ?>
+    </body>
+</html>
