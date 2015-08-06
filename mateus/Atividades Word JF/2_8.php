31d76010635f9b25b8905e3b3f606902cbd68e16
mateusmotti <mateus.aviz.motti@gmail.com>
2015-08-05 20:19:49 -0400

    salario reajustado

--- mateus/Atividades Word JF/2_8.php
+++ mateus/Atividades Word JF/2_8.php
@@ -5,19 +5,13 @@
     </head>
     <body>
         <form method="post" action="2_8.php">
-            
+            <label>Seu salário:</label>
+            <input type="text" name="salario" id="salario"/>
+            <input type="submit" name="enviar" id="enviar"/>
         </form>
         <?php
              if(isset($_POST['enviar'])){
-                 $sexo = $_POST['sexo'];
-                 $altura = $_POST['altura'];
-                 if(($sexo = "f") && ($sexo = "feminino") && ($sexo = "mulher")){
-                     $pesoideal = 62.1 * $altura;
-                     echo "Seu peso ideal é $pesoideal";
-                 }elseif(($sexo = "m") && ($sexo = "masculino") && ($sexo = "homem")){
-                     $pesoideal = 72.7 * $altura;
-                     echo "Seu peso ideal é $pesoideal";
-                 }
+                 
              }
         ?>
     </body>
