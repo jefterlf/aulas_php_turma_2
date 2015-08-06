491e2c5c50dbc5417a123a26138a7f2f709e6754
mateusmotti <mateus.aviz.motti@gmail.com>
2015-08-05 20:50:54 -0400

    2_10

--- mateus/Atividades Word JF/2_10.php
+++ mateus/Atividades Word JF/2_10.php
@@ -5,10 +5,21 @@
     </head>
     <body>
         <form method="post" action="2_10.php">
-            
+            <label>Número:</label>
+            <input type="number" name="numero" id="numero"/>
+            <input type="submit" name="enviar" id="enviar"/>
         </form>
         <?php
-             
+          if(isset($_POST['enviar'])){   
+                $numero = $_POST['numero'];
+                $cont = 1;
+                echo $numero. "! = ";
+                while($cont <= $numero){
+                    $fatorial = $cont * $numero;
+                    echo "$fatorial ";
+                    $cont++;
+                }
+          }      
         ?>
     </body>
 </html>
