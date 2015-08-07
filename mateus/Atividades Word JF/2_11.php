165fb2249a8304457e4f443e571ccbe3bf9ade37
mateusmotti <mateus.aviz.motti@gmail.com>
2015-08-06 21:44:01 -0400

    2_11

---
+++ mateus/Atividades Word JF/2_11.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <title>Intermediário</title>
+    </head>
+    <body>
+        <form method="post" action="2_11.php">
+            <label>Número:</label>
+            <input type="number" name="numero" id="numero"/>
+            <input type="submit" name="enviar" id="enviar"/>
+        </form>
+        <?php
+          if(isset($_POST['enviar'])){   
+                
+          }
+        ?>
+    </body>
+</html>
