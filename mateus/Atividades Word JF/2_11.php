31f62b4385a178b00bf8ea11f19042c2fa717288
mateusmotti <mateus.aviz.motti@gmail.com>
2015-08-06 21:46:42 -0400

    ajeitado 2_11

--- mateus/Atividades Word JF/2_11.php
+++ mateus/Atividades Word JF/2_11.php
@@ -5,12 +5,16 @@
     </head>
     <body>
         <form method="post" action="2_11.php">
-            <label>Número:</label>
-            <input type="number" name="numero" id="numero"/>
+            <label>1º Número:</label>
+            <input type="number" name="numero1" id="numero1"/>
+            <label>2º Número:</label>
+            <input type="number" name="numero2" id="numero2"/>
             <input type="submit" name="enviar" id="enviar"/>
         </form>
         <?php
           if(isset($_POST['enviar'])){   
+                $numero1 = $_POST['numero1'];
+                $numero2 = $_POST['numero2'];
                 
           }
         ?>
